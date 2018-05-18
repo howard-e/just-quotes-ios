1c2788ca17eee4612eef1f9f3b32074bb8e57a54
Howard Edwards <howarde.edwards@gmail.com>
2018-05-18 09:44:38 -0500

    Initial Commit

---
+++ Just Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Just Quotes
+//
+//  Created by Howard Edwards on 5/18/18.
+//  Copyright © 2018 Heed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
